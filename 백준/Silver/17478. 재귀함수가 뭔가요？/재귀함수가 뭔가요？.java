0b6b4a14cec03b1cd1a249dcb3ce3e21fa9403eb
노주희 <juhee0924@g.skku.edu>
2026-01-08 15:38:05 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -34,30 +34,37 @@ class Main {
 		n = Integer.parseInt(br.readLine());
 
 		sb
-		.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n")
-		.append("\"재귀함수가 뭔가요?\"").append("\n");
+		.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
 
-		func(n, "");
-		
-		sb.append("라고 답변하였지.").append("\n");
-		
+		func(0);
 		System.out.println(sb);
 	}
 
-	void func(int k, String suffix) {
-		if (k == 0) {
+	void func(int k) {
+		StringBuilder tmp = new StringBuilder();
+		for (int i = 0; i < k; i++) {
+			tmp.append(bar);
+		}
+		String prefix = tmp.toString();
+
+		sb
+		.append(prefix).append("\"재귀함수가 뭔가요?\"").append("\n");
+
+		if (k == n) {
 			sb
-			.append(suffix).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+			.append(prefix).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n")
+			.append(prefix).append("라고 답변하였지.").append("\n");	
 			return;
 		}
 
 		sb
-		.append(suffix).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n")
-		.append(suffix).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n")
-		.append(suffix).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n")
-		.append(suffix).append("____\"재귀함수가 뭔가요?\"").append("\n");
-		func(--k, suffix + bar);
+		.append(prefix).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n")
+		.append(prefix).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n")
+		.append(prefix).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
+		
+		func(k + 1);
+
 		sb
-		.append(suffix).append("____라고 답변하였지.").append("\n");
+		.append(prefix).append("라고 답변하였지.").append("\n");
 	}
 }
