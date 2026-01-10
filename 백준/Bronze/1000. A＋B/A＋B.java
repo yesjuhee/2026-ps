635165169f403b9f3a5ff2c543ab702a098ecd3a
노주희 <juhee0924@g.skku.edu>
2026-01-10 18:18:57 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		new Main().solution();
+	}
+
+	public void solution() throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a + b);
+	}
+}
