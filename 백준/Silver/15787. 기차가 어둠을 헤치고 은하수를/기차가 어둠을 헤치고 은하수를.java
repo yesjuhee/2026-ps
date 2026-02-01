96285fd46382d6618cdea0d4be07cfc07d534158
노주희 <juhee0924@g.skku.edu>
2026-02-01 18:14:57 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 344 ms, Memory: 38664 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -36,32 +36,21 @@ class Main {
             } else if (cmd == 2) {
                 int x = Integer.parseInt(st.nextToken());
                 // x 번째 좌석 하차
-                int mask = 0;
-                for (int j = 0; j < 20; j++) {
-                    mask = (mask << 1) | ((j == 20 - x) ? 0 : 1);
-                }
+                int mask = ~(1 << (x-1));
                 states[t] = states[t] & mask;
             } else if (cmd == 3) {
                 states[t] = (states[t] << 1) & 0xFFFFF;
             } else {
                 states[t] = states[t] >> 1;
             }
-            // 모든 기차의 상태 출력
-            // System.out.printf("%d번째 명령\n", i + 1);
-            // for (int j = 0; j < n; j++) {
-            //     System.out.printf("%d: %032d\n", j + 1, Integer.parseInt(Integer.toBinaryString(states[j])));
-            // }
         }
 
         // n개 기차 상태 확인 + 결과 카운트
-        List<Integer> results = new ArrayList<>();
+        Set<Integer> results = new HashSet<>();
         for (int i = 0; i < n; i++) {
-            int train = states[i];
-            if (results.contains(train)) continue;
-            results.add(train);
-            result++;
+            results.add(states[i]);
         }
 
-        System.out.println(result);
+        System.out.println(results.size());
 	}
 }
