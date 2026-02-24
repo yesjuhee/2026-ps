d02ce840b3a75498a109cfb407463c761181c699
노주희 <juhee0924@g.skku.edu>
2026-02-24 16:03:59 +0900

    [Silver III] Title: N번째 큰 수, Time: 768 ms, Memory: 272880 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -11,24 +11,32 @@ class Main {
         전체 숫자 리스트로 저장 -> O(N^2)
         정렬 -> O(N^2logN^2)
         조회 -> O(1)
-        가능하겠는데? 근데 아슬아슬함 (8천만)
+        가능하겠는데? 근데 아슬아슬함
+
+        우선순위 큐 활용 
+        전체 숫자 저장 
+        -> 하나씩 넣으면 O(N^2logN^2)
+        -> 전체 입력 받고 일괄 생성하면 -> O(N^2)
         */
         new Main().solution();
 	}
 
     public void solution() throws IOException {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((e1, e2) -> e2 - e1);
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n * n];
+        
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                arr[i * n + j] = num;
+                pq.add(num);
             }
         }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            pq.poll();
+        }
 
-        System.out.println(arr[n * n - n]);
+        System.out.println(pq.poll());
     }
 }
