3745ebe2929332735775ae166ac50e285d7df1a0
노주희 <juhee0924@g.skku.edu>
2026-02-27 21:56:20 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+        new Main().solution();
+	}
+
+    public void solution() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 0; j < n - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
