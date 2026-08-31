b984559ace4659dffefc4cf56eb47748bc96262e
노주희 <juhee0924@g.skku.edu>
2026-08-31 20:40:47 +0900

    [level 2] Title: 올바른 괄호, Time: 9.21 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,26 +3,20 @@ import java.util.*;
 class Solution {
     
     boolean solution(String s) {
-        Deque<Character> stack1 = new ArrayDeque<>();
-        Deque<Character> stack2 = new ArrayDeque<>();
+        Deque<Character> stack = new ArrayDeque<>();
         
         for(char c: s.toCharArray()) {
-            stack1.push(c);
-        }
-        
-        while(!stack1.isEmpty()) {
-            stack2.push(stack1.pop());
-            if (stack1.isEmpty()) return false;
-            
-            while (
-                !stack1.isEmpty() && !stack2.isEmpty() &&
-                stack1.peek() == '(' && stack2.peek() == ')') {
-                // 짝이 맞음 
-                stack1.pop();
-                stack2.pop();
+            if (stack.isEmpty()) {
+                stack.push(c); 
+                continue;
+            }
+            if (stack.peek() == '(' && c == ')') {
+                stack.pop();
+            } else {
+                stack.push(c);
             }
         }
-
-        return stack2.isEmpty();
+        
+        return stack.isEmpty();
     }
 }
